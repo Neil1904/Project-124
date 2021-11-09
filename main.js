a485b5c2a6057db30923c9ea01c864370b0f082b
Neil1904 <75314458+Neil1904@users.noreply.github.com>
2021-11-09 17:37:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 450);
+
+    canvas = createCanvas(550, 420);
+    canvas.position(560, 80);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotposes);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is initialized');
+}
+
+function gotposes(results) {
+    if (results > 0) {
+        console.log(results);
+    }
+}
